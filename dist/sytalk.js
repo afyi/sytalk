69941eb11417a385c91c4526f71048c45a019ba4
在下猫南北 <rakiy@qq.com>
2022-11-20 19:58:10 +0800

    bugfix 1.1.1

--- dist/sytalk.js
+++ dist/sytalk.js
@@ -161,7 +161,7 @@ t.exports=function(t){return null!=t&&(n(t)||r(t)||!!t._isBuffer)}},function(t,e
 'use strict'
 
 // 版本号
-const atVersion = "1.1.0";
+const atVersion = "1.1.1";
 
 // 表情包，兼容sytalk的
 const atEmoji = {
